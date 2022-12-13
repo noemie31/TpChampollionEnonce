f03ba3ab0279fe86472b15999e8abbed284016d9
noemie31 <99802192+noemie31@users.noreply.github.com>
2022-12-13 12:20:15 +0100

    Add files via upload

--- src/test/java/champollion/ChampollionJUnitTest.java
+++ src/test/java/champollion/ChampollionJUnitTest.java
@@ -2,16 +2,21 @@ package champollion;
 
 import org.junit.jupiter.api.*;
 import static org.junit.jupiter.api.Assertions.*;
+import java.util.Date;
 
 public class ChampollionJUnitTest {
 	Enseignant untel;
 	UE uml, java;
+	Intervention interventiontd,interventioncm,interventiontp,interventionjava;
 		
 	@BeforeEach
 	public void setUp() {
 		untel = new Enseignant("untel", "untel@gmail.com");
 		uml = new UE("UML");
-		java = new UE("Programmation en java");		
+		java = new UE("Programmation en java");
+		interventiontd = new Intervention(new Date(),10,2,new Salle("102",35),TypeIntervention.TD,untel,uml);
+		interventioncm =  new Intervention(new Date(),10,2,new Salle("102",35),TypeIntervention.CM,untel,uml);
+		interventiontp =  new Intervention(new Date(),10,2,new Salle("102",35),TypeIntervention.TP,untel,uml);
 	}
 	
 
@@ -36,5 +41,50 @@ public class ChampollionJUnitTest {
                          "L'enseignant doit maintenant avoir 30 heures prévues pour l'UE 'uml'");		
 		
 	}
+
+	@Test
+	public void testHeurePrevues(){
+		untel.ajouteEnseignement(uml,0,10,0);
+		untel.ajouteEnseignement(java,0,5,0);
+		assertEquals(15,untel.heuresPrevues());
+	}
+
+	@Test
+	public void testAjoutIntervention(){
+		untel.ajouteEnseignement(uml,0,10,0);
+		untel.ajouterIntervention(interventiontd);
+		assertEquals(1,untel.getInterventions().size());
+		try{
+			untel.ajouterIntervention(interventionjava);
+		}catch (Exception e){
+
+		}
+	}
+
+	@Test
+	public void testHeureAPlanifier(){
+		untel.ajouteEnseignement(uml,20,20,20);
+		untel.ajouterIntervention(interventiontd);
+		untel.ajouterIntervention(interventioncm);
+		untel.ajouterIntervention(interventiontp);
+		assertEquals(18,untel.resteAPlanifier(uml,TypeIntervention.TD));
+		assertEquals(18,untel.resteAPlanifier(uml,TypeIntervention.CM));
+		assertEquals(18,untel.resteAPlanifier(uml,TypeIntervention.TP));
+		//l'intervention dure 2h donc on enleve 2h des 20h de TD, et la valeur attendue est la temps qu'il reste a planifier en TD
+		try{
+			//untel n'enseigne pas de java
+			untel.resteAPlanifier(java,TypeIntervention.TD);
+		}catch (Exception e){
+
+		}
+	}
+
+	@Test
+	public void testEnSousService(){
+		untel.ajouteEnseignement(uml,10,10,10);
+		untel.ajouterIntervention(interventiontd);
+		assertTrue(untel.enSousService());
+	}
+
 	
 }
